d5d2fe00701d1e3393e7dbec6a9e75ddac75ca60
chenjian <hi.cj@outlook.com>
2020-07-29 10:06:45 +0800

    feat: 调整首页界面

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,6 @@ Page({
   data: {
     StatusBar: app.globalData.StatusBar,
     CustomBar: app.globalData.CustomBar,
-    motto: 'Hi 开发者！',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
